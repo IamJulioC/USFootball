f76d967415661d0fbcebf67e7e128355bdb23a18
Júlio <iamjulioc@live.com>
2026-05-05 22:56:36 -0300

    feat: add CRUD routes (GET, POST, PUT, PATCH)

--- Program.cs
+++ Program.cs
@@ -7,6 +7,51 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data S
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/teams", async (AppDbContext db) =>
+{
+    return await db.Teams.ToListAsync();
+});
+
+app.MapGet("/teams/{id}", async (int id, AppDbContext db) =>
+{
+    var team = await db.Teams.FindAsync(id);
+    return team is not null ? Results.Ok(team) : Results.NotFound("Time não encontrado");
+});
+
+app.MapPost("/teams", async (AppDbContext db, Team newTeam) =>
+{
+    db.Teams.Add(newTeam);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"O time {newTeam.Nome} foi adicionado com sucesso", newTeam);
+});
+
+app.MapPut("/teams/{id}", async (int id, AppDbContext db, Team updatedTeam) =>
+{
+    var team = await db.Teams.FindAsync(id);
+    if (team is null ) return Results.NotFound("Time não encontrado");
+
+    team.Nome = updatedTeam.Nome;
+    team.Cidade = updatedTeam.Cidade;
+    team.SuperBowlsGanhos = updatedTeam.SuperBowlsGanhos;
+    team.JogosForaDoPais = updatedTeam.JogosForaDoPais;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(team);
+});
+
+app.MapPatch("/teams/{id}", async (int id, AppDbContext db, Team updatedFields) =>
+{
+    var team = await db.Teams.FindAsync(id);
+    if (team is null) return Results.NotFound("Time não encontrado");
+
+    if (!string.IsNullOrEmpty(updatedFields.Nome)) team.Nome = updatedFields.Nome;
+    if (!string.IsNullOrEmpty(updatedFields.Cidade)) team.Cidade = updatedFields.Cidade;
+    if (updatedFields.SuperBowlsGanhos != 0) team.SuperBowlsGanhos = updatedFields.SuperBowlsGanhos;
+    if (updatedFields.JogosForaDoPais != 0) team.JogosForaDoPais = updatedFields.JogosForaDoPais;
+
+    await db.SaveChangesAsync();
+    return Results.Ok($"Time {team.Nome} atualizado com sucesso");
+});
 
 app.Run();
